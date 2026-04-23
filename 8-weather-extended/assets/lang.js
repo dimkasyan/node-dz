93e6d9f40b29829085ae0f9550234ee697650236
Dmitriy Petrov <strajmag95@gmail.com>
2026-04-23 17:16:20 +0300

    8-weather-extended done

---
+++ 8-weather-extended/assets/lang.js
@@ -0,0 +1,18 @@
+const languages = {
+    ru: {
+        weatherCity:'Погода в городе',
+        temp: 'Температура',
+        humidity: 'Влажность',
+        windSpeed: 'Скорость ветра',
+        feelsLike: 'ощущается как',
+    },
+    en: {
+        weatherCity: 'Weather in the city',
+        temp: 'Temperature',
+        humidity: 'Humidity',
+        windSpeed: 'Wind speed',
+        feelsLike: 'It feels like',
+    }
+};
+
+export { languages };
