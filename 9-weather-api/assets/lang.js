502e7054f249d92df770094460a8bebb3a257310
Dmitriy Petrov <strajmag95@gmail.com>
2026-07-22 14:55:14 +0300

    make task 9-weather-api

---
+++ 9-weather-api/assets/lang.js
@@ -0,0 +1,22 @@
+const languages = {
+    ru: {
+        weatherCity:'Погода в городе',
+        temp: 'Температура',
+        humidity: 'Влажность',
+        windSpeed: 'Скорость ветра',
+        feelsLike: 'ощущается как',
+        icon: 'Иконка погоды',
+        description: 'Описание погоды',
+    },
+    en: {
+        weatherCity: 'Weather in the city',
+        temp: 'Temperature',
+        humidity: 'Humidity',
+        windSpeed: 'Wind speed',
+        feelsLike: 'It feels like',
+        icon: 'Weather icon',
+        description: 'Weather description',
+    }
+};
+
+export { languages };
